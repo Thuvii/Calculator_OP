7cdacc9db27c28343d101406fc242b7e370a6fd7
ThuVi <buihhh321@gmail.com>
2024-09-04 01:35:29 -0400

    design front-end and test btn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("testing javafile")
+
+
+let btnNumber = document.querySelectorAll('.btn-number');
+
+
+
+btnNumber.forEach((btn) => {
+    btn.addEventListener("click", () =>{
+        console.log(btn.innerText)
+    });
+})
