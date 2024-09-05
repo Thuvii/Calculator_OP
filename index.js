b16e1612addcba10399544c1c733a1b3654df411
Thuvii <buihhh321@gmail.com>
2024-09-04 23:09:58 -0400

    test keyboard function

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 
 
 let btnNumber = document.querySelectorAll('.btn-number');
-
+let opDisplay = document.querySelector('.operationScreen');
 
 
 btnNumber.forEach((btn) => {
     btn.addEventListener("click", () =>{
-        console.log(btn.innerText)
+        console.log(typeof(btn.innerText));
     });
 })
+document.addEventListener('keypress', (a) => {
+  opDisplay.innerText += `${a.key}`;
+});
+
