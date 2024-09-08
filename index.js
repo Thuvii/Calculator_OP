0daed1a0502240357327b861ce14e00ffbbd47e6
Thuvii <buihhh321@gmail.com>
2024-09-08 01:50:06 -0400

    making chage sign function

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 //  opDisplay.innerText += `${a.key}`;
 //});
 
-let number1 = 0;
-let number2 = 0;
+let number1 = "";
+let number2 = "";
 let oper = "";
 let checkNum2 = false;
-
 
 let btnNumber = document.querySelectorAll(".btn-number");
 let opDisplay = document.querySelector(".operationScreen");
@@ -15,22 +14,23 @@ let equal = document.querySelector(".result-btn");
 let resDisplay = document.querySelector(".resultScreen");
 let cleanScreen = document.querySelector(".clear-btn");
 let dotBtn = document.querySelector(".dot-btn");
+let signBtn = document.querySelector(".btn-sign");
 
 function getNumber() {
   btnNumber.forEach((btn) => {
     btn.addEventListener("click", () => {
       if (!checkNum2) {
-        if(btn.innerText == "+/-"){
-          number1 = changeSign(number1);
-        }else{
+        if (btn.innerText == "+/-") {
+          changeSignBtn(number1);
+        } else {
           number1 += btn.innerText;
           checkdot(number1);
         }
         opDisplay.innerText = number1;
       } else {
-        if(btn.innerText == "+/-"){
-          number2 = changeSign(number2);
-        }else{
+        if (btn.innerText == "+/-") {
+          changeSignBtn(number2);
+        } else {
           number2 += btn.innerText;
           checkdot(number2);
         }
@@ -40,9 +40,17 @@ function getNumber() {
   });
 }
 
-function changeSign(a){
-  return parseFloat(a) * -1
+function changeSignBtn(number) {
+  if (number == "") {
+    signBtn.disabled = true;
+    return;
+  } else {
+    signBtn.disabled = false;
+    number = parseFloat(number) * -1;
+    return number;
+  }
 }
+
 function getOper() {
   operation.forEach((btn) => {
     btn.addEventListener("click", () => {
@@ -55,14 +63,13 @@ function getOper() {
   });
 }
 
-function checkdot(number){
-  if(number.includes(".")){
+function checkdot(number) {
+  if (number.includes(".")) {
     dotBtn.disabled = true;
-  }else{
+  } else {
     dotBtn.disabled = false;
   }
 }
-
 
 function calculate() {
   let result = 0;
@@ -80,40 +87,38 @@ function calculate() {
         result = n1 * n2;
         break;
       case "÷":
-        if(n2 == 0){
+        if (n2 == 0) {
           resDisplay.innerText = "Can't divide by 0";
           return;
-        }else{
+        } else {
           result = n1 / n2;
         }
         break;
       case "%":
-        result = (n1/n2) * 100;
+        result = (n1 / n2) * 100;
         break;
     }
-    result = parseFloat(result.toFixed(10))
+    result = parseFloat(result.toFixed(10));
     resDisplay.innerText = result;
     clearAll();
   }
 }
 
 function clearAll() {
-  number1 = 0;
-  number2 = 0;
+  number1 = "";
+  number2 = "";
   oper = "";
   checkNum2 = false;
   dotBtn.disabled = false;
 }
-function clean(){
- resDisplay.innerText = ""; 
- opDisplay.innerText = "";
- clearAll();
+function clean() {
+  resDisplay.innerText = "0";
+  opDisplay.innerText = "";
+  clearAll();
   dotBtn.disabled = false;
 }
-
 
 equal.addEventListener("click", calculate);
 cleanScreen.addEventListener("click", clean);
 getNumber();
 getOper();
-
